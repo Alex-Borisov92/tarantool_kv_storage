47e92b5161b83e3bb7895877e0f575d98f3747c1
Alex-Borisov92 <56392877+alex-borisov92@users.noreply.github.com>
2019-10-12 14:24:17 -0700

    added final logic for rps_limit

--- kv.lua
+++ kv.lua
@@ -62,55 +62,54 @@ end
 
 --TODO finish it - doesnt work
 
---- Model storing requests count for ip and ts
---local request_count = {
---    space_name = 'request_count',
---    model = {
---        ip = 1,
---        ts = 2,
---        cnt = 3,
---    },
---}
---
---test_scheme = box.schema.space.create(request_count.space_name, {
---    if_not_exists = true,
---    temporary = true,
---})
---test_scheme:create_index('primary', {
---    type = 'hash',
---    parts = {request_count.model.ip, 'string', request_count.model.ts, 'unsigned'},
---    if_not_exists = true,
---})
+-- Model storing requests count for ip and ts
+local request_count = {
+    space_name = 'request_count',
+    model = {
+        ip = 1,
+        ts = 2,
+        cnt = 3,
+    },
+}
+
+test_scheme = box.schema.space.create(request_count.space_name, {
+    if_not_exists = true,
+    temporary = true,
+})
+test_scheme:create_index('primary', {
+    type = 'hash',
+    parts = {request_count.model.ip, 'string', request_count.model.ts, 'unsigned'},
+    if_not_exists = true,
+})
 
 
 
 
---function limited_rps(handler, rps_limit)
---    return function (req)
---        local ts = os.time()
---        local rows = box.space[request_count.space_name]:select({req.peer.host, ts})
---        if #rows ~= 0 and rows[1][request_count.model.cnt] == rps_limit then
---            local resp = req:render({text = 'Too Many Requests'})
---            resp.status = 429
---            return resp
---        end
---        box.space[request_count.space_name]:upsert({req.peer.host, ts, 1}, {{'+', request_count.model.cnt, 1}})
---        log.info(tostring(req))
---        return handler(req)
---    end
---end
+function limited_rps(handler, rps_limit)
+    return function (req)
+        local ts = os.time()
+        local rows = box.space[request_count.space_name]:select({req:peer()['host'], ts})
+        if #rows ~= 0 and rows[1][request_count.model.cnt] == rps_limit then
+            local resp = req:render({text = 'Too Many Requests'})
+            resp.status = 429
+            return resp
+        end
+        box.space[request_count.space_name]:upsert({req:peer()['host'], ts, 1}, {{'+', request_count.model.cnt, 1}})
+        return handler(req)
+    end
+end
 log.info('creating http-server...')
 local server = srv.new(host, port,{ log_requests = true })
 
 local httpd = router.new()
 server:set_router(httpd)
 
-httpd:route({ path = '/kv/:id', method = 'GET' }, hdl.get)
-httpd:route({ path = '/kv/:id', method = 'DELETE' }, hdl.del)
-httpd:route({ path = '/kv/:id', method = 'PUT' }, hdl.put)
-httpd:route({ path = '/kv', method = 'POST' },hdl.post)
+httpd:route({ path = '/kv/:id', method = 'GET' }, limited_rps(hdl.get,2))
+httpd:route({ path = '/kv/:id', method = 'DELETE' }, limited_rps(hdl.del,2))
+httpd:route({ path = '/kv/:id', method = 'PUT' },limited_rps(hdl.put,2))
+httpd:route({ path = '/kv', method = 'POST' },limited_rps(hdl.post,2))
 httpd:hook('after_dispatch', function(req, rsp)
 	-- log all correct operations
-	log.info(string.format("%s %s %s %s", rsp.status, req.method, req.path, req:read_cached()))
+	log.info(string.format("%s %s %s %s %s", rsp.status, req.method, req.path, req:read_cached()),req.peer.host)
 end)
 server:start()
